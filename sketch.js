8fe6030fc1a875b276ece4bc6e44afa861934719
NVishnu23 <73948719+NVishnu23@users.noreply.github.com>
2021-01-15 19:32:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,30 +2,36 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-var ground;
-var Log1;
-
-function preload()
-{
-	
-}
 
 function setup() {
-	createCanvas(800,700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
-
-	ground = createSprite(width/2, height-35, width,10);
-	ground.shapeColor = color("yellow");
-
-	log1 = new Log(550,610,13,70);
-
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
 	
-	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
 	Engine.run(engine);
+	//Render.run(render);
   
 }
 
@@ -33,13 +39,23 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  ground.display();
-  log1.display();
-  
-
-
-  drawSprites();
  
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
 }
 
 
